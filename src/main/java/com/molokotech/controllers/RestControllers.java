b66eee7fcd0b5c8bbf8d7cf68dda7c1413da9520
berlot83 <berlot83@yahoo.com.ar>
2018-12-26 08:16:38 -0300

    test

--- src/main/java/com/molokotech/controllers/RestControllers.java
+++ src/main/java/com/molokotech/controllers/RestControllers.java
@@ -160,16 +160,11 @@ public class RestControllers {
 		
 		MercadoPago.SDK.configure("4306840655072811", "uT7N5Y0B5lj9rophOy50yEh3EkEJo7jO");
 		String accessToken = MercadoPago.SDK.getAccessToken();
-
+System.out.println(accessToken);
+		
 		MPApiResponse api = MercadoPago.SDK.Get("https://api.mercadopago.com/v1/payments/4391970308?access_token="+accessToken);
 		System.out.println(api);
 		
-		System.out.println(api.getStringResponse());
-		
-		Payment p = new Payment();
-		System.out.println(p.getPayer().getEmail());
-		
-		p.save();
 //		
 //		System.out.println(api.getPayload());
 //		System.out.println(api.getStringResponse());
@@ -185,7 +180,6 @@ public class RestControllers {
 //		
 //		
 		
-		System.out.println(MercadoPago.SDK.Get("/v1/customers/"+id+"?access_token="+accessToken));
 		
 		
 //		System.out.println("hello world");
