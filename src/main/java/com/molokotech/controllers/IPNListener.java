fdb2f0a00f46e8405dc3714192e9a7b30f22f846
berlot83 <berlot83@yahoo.com.ar>
2019-01-22 18:54:12 -0300

    sandbox problems

--- src/main/java/com/molokotech/controllers/IPNListener.java
+++ src/main/java/com/molokotech/controllers/IPNListener.java
@@ -5,13 +5,16 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.paypal.ipn.IPNMessage;
 
+@RestController
+@Controller
 public class IPNListener {
-
-	
 	
 	@RequestMapping(value = "/cgi-bin/webscr", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.ALL_VALUE})
 	public ResponseEntity<?> listener(HttpServletRequest request, HttpServletRequest response) {
