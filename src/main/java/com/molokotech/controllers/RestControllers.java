80069c50103bcb12dd565c6f5200f1d3bfa8d684
berlot83 <berlot83@yahoo.com.ar>
2019-01-22 19:38:58 -0300

    almost success sandbox paypal

--- src/main/java/com/molokotech/controllers/RestControllers.java
+++ src/main/java/com/molokotech/controllers/RestControllers.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import javax.mail.Message;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.coyote.http2.Http2Protocol;
 import org.codehaus.jettison.json.JSONArray;
@@ -353,18 +354,76 @@ public class RestControllers {
 		return result;
 	}
 	
-	
-	@RequestMapping(value = "/notifications-paypal", method = RequestMethod.POST, produces = "application/json")
-	public ResponseEntity<?> notificationsPayPal(Map<String, String> ipnMap) throws JSONException, Exception {
-		
-		Map<String,String> configMap = new HashMap<String,String>();
+	/* PayPal listener and sender of PrepaidQR code to new User */
+	@RequestMapping(value = "/cgi-bin/webscr", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.ALL_VALUE})
+	public ResponseEntity<?> listener(HttpServletRequest request, HttpServletRequest response) {
+
+		Map<String, String> configMap = new HashMap<>();
 		configMap.put("mode", "sandbox");
-		// Account Credential
-		configMap.put("acct1.UserName", "jb-us-seller_api1.paypal.com");
-		configMap.put("acct1.Password", "WX4WTU3S8MY44S7F");
-		configMap.put("acct1.Signature", "AFcWxV21C7fd0v3bYYYRCpSSRl31A7yDhhsPUU2XhtMoZXsWHFxu-RWy");
+		configMap.put("acct1.UserName", "berlot83_api1.yahoo.com.ar");
+		configMap.put("acct1.Password", "DUU3SLX82NFKPSEQ");
+		configMap.put("acct1.Signature", "AVO4ngS4QK8KXIH04mEbcSuZHWY4AD2YGHkFDxfa1O13qrWw-RK31nTp");
 		
-		return new ResponseEntity<>(HttpStatus.CREATED);
+		IPNMessage ipnlistener = new IPNMessage(request, configMap);
+		boolean isIpnVerified = ipnlistener.validate();
+		String transactionType = ipnlistener.getTransactionType();
+		
+		Map<String, String> map = ipnlistener.getIpnMap();
+		
+		System.out.println("******* IPN (name:value) pair : " + map + "  " + "######### TransactionType : "
+				+ transactionType + "  ======== IPN verified : " + isIpnVerified);
+		
+		System.out.println("");
+		System.out.println("");
+		String payerEmail = map.get("payer_email");
+		System.out.println(payerEmail);
+
+		/* Check if is verified */
+		if (isIpnVerified) {
+			System.out.println("Verified IPN");
+		/* End check */
+
+			/* Start checking, sending and reemplacing sellecdOnline java attribute*/
+			PrepaidQR prepaidQR = null;
+			List<PrepaidQR> list = prepaidQrService.findAllPrepaidQR();
+			
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).getSelledOnline().equals("En venta")) {
+					prepaidQR = list.get(i);
+					System.out.println(list.get(i).getSelledOnline());
+					System.out.println(list.get(i).getId());
+					break;
+				}else {
+					System.out.println("No match with 'En venta'");
+				}
+			}
+			/* End selecting 'En venta match' */
+
+			/* Send email with id start */
+			String idPrepaidQR = prepaidQR.getId().toString();
+			
+			MimeMessagePreparator preparator = new MimeMessagePreparator() {
+				public void prepare(MimeMessage mimeMessage) throws Exception {
+					mimeMessage.setSubject("Código QR adquirido.");
+					mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(payerEmail));
+					mimeMessage.setFrom(new InternetAddress("info@molokotech.com"));
+					mimeMessage.setText("Gracias por tu compra, el id del QR es = " + idPrepaidQR +", el mail asignado es "+ payerEmail +",  lo que tenés que hacer es ir a nuestra web, ingresar a Pet-QR ==> Activar un QR prepago ==> Ingresá el código que recibiste junto con el mail y listo, fijate de completar todo lo que puedas del formulario. Una vez completo Tocá el botón rojo y cuando te salga el QR probalo desde la PC, si sale todo bien tendrías que ver todos los datos que pusiste.");
+				}
+			};
+
+			try {
+				this.emailSender.send(preparator);
+			} catch (MailException ex) {
+				System.err.println(ex.getMessage());
+			}
+			/* Send email with id end */
+			
+		}		
+		else{
+			System.out.println("Not a valid IPN Request!");
+		}
+		
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 }
